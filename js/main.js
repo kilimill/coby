b8d50765881d4ae04f0c75838a0ed96869ef1e2e
kilimill <kilimill2585@gmail.com>
2020-09-19 15:21:24 +0700

    start page

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$("#plastic-btn").click(function () {
+  $(".filter__item").hide();
+  $(".plastic").show();
+});
+
+$("#glass-btn").click(function () {
+  $(".filter__item").hide();
+  $(".glass").show();
+});
+
+$("#paper-btn").click(function () {
+  $(".filter__item").hide();
+  $(".paper").show();
+});
+
+$("#ceramics-btn").click(function () {
+  $(".filter__item").hide();
+  $(".ceramics").show();
+});
+
+$("#textile-btn").click(function () {
+  $(".filter__item").hide();
+  $(".textile").show();
+});
+
+$("#tree-btn").click(function () {
+  $(".filter__item").hide();
+  $(".tree").show();
+});
+
+$("#metal-btn").click(function () {
+  $(".filter__item").hide();
+  $(".metal").show();
+});
+
+$("#orgaplastic-btn").click(function () {
+  $(".filter__item").hide();
+  $(".orgaplastic").show();
+});
+
+$("#marble-btn").click(function () {
+  $(".filter__item").hide();
+  $(".marble").show();
+});
+
+$("#skin-btn").click(function () {
+  $(".filter__item").hide();
+  $(".skin").show();
+});
+
+$("#material-btn").click(function () {
+  $(".filter__item").hide();
+  $(".material").show();
+});
