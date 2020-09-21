d3c1114c6d6a5cfa00bb45949cf7ec428e253bd2
kilimill <kilimill2585@gmail.com>
2020-09-21 18:21:28 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,54 +1,59 @@
-$("#plastic-btn").click(function () {
+$('.menu-item__btn').on('click', function () {
+  $(this).addClass('--on');
   $(".filter__item").hide();
-  $(".plastic").show();
 });
 
-$("#glass-btn").click(function () {
-  $(".filter__item").hide();
-  $(".glass").show();
+$("#plastic-btn").on('click', function () {
+  $(".plastic").addClass('active');
+  $('.active').show();
 });
 
-$("#paper-btn").click(function () {
-  $(".filter__item").hide();
-  $(".paper").show();
+$("#glass-btn").on('click', function () {
+  $(".glass").addClass('active');
+  $('.active').show();
 });
 
-$("#ceramics-btn").click(function () {
-  $(".filter__item").hide();
-  $(".ceramics").show();
+$("#paper-btn").on('click', function () {
+  $(".paper").addClass('active');
+  $('.active').show();
 });
 
-$("#textile-btn").click(function () {
-  $(".filter__item").hide();
-  $(".textile").show();
+$("#ceramics-btn").on('click', function () {
+  $(".ceramics").addClass('active');
+  $('.active').show();
 });
 
-$("#tree-btn").click(function () {
-  $(".filter__item").hide();
-  $(".tree").show();
+$("#textile-btn").on('click', function () {
+  $(".textile").addClass('active');
+  $('.active').show();
 });
 
-$("#metal-btn").click(function () {
-  $(".filter__item").hide();
-  $(".metal").show();
+$("#tree-btn").on('click', function () {
+  $(".tree").addClass('active');
+  $('.active').show();
 });
 
-$("#orgaplastic-btn").click(function () {
-  $(".filter__item").hide();
-  $(".orgaplastic").show();
+$("#metal-btn").on('click', function () {
+  $(".filter__item").addClass('active');
+  $('.active').show();
 });
 
-$("#marble-btn").click(function () {
-  $(".filter__item").hide();
-  $(".marble").show();
+$("#orgaplastic-btn").on('click', function () {
+  $(".orgaplastic").addClass('active');
+  $('.active').show();
 });
 
-$("#skin-btn").click(function () {
-  $(".filter__item").hide();
-  $(".skin").show();
+$("#marble-btn").on('click', function () {
+  $(".marble").addClass('active');
+  $('.active').show();
 });
 
-$("#material-btn").click(function () {
-  $(".filter__item").hide();
-  $(".material").show();
+$("#skin-btn").on('click', function () {
+  $(".skin").addClass('active');
+  $('.active').show();
+});
+
+$("#material-btn").on('click', function () {
+  $(".material").addClass('active');
+  $('.active').show();
 });
